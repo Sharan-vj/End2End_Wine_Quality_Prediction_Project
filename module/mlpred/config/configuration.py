4e4ccf1b58ce99027b957e2641a863ea9a74d29b
sharan <sharanvj678@gmail.com>
2023-12-29 20:25:33 +0530

    Data validation added

--- module/mlpred/config/configuration.py
+++ module/mlpred/config/configuration.py
@@ -1,6 +1,7 @@
 from mlpred.constant import *
 from mlpred.utils.common import read_yaml, create_directories
 from mlpred.entity.config_entity import DataIngestionConfig
+from mlpred.entity.config_entity import DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -10,7 +11,7 @@ class ConfigurationManager:
         schema_filepath = SCHEMA_FILE_PATH):
 
         self.config = read_yaml(config_filepath)
-        
+        self.schema = read_yaml(schema_filepath)
 
         create_directories([self.config.artifacts_root])
 
@@ -29,3 +30,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir=config.unzip_data_dir,
+            all_schema=schema
+        )
+
+        return data_validation_config
